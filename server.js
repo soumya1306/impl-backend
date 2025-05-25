ec4b284805598ad8fbf68ffb530fedcf6c587c62
soumya1306 <soumya1317@gmail.com>
2025-05-25 19:17:07 +0530

    send-email api format updated

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
 
 
 app.get("/", (req, res) => {
-  
+  res.status(200).json({message: "Welcome to impl backend"})
 });
 
 app.post("/api/send-email", async (req, res) => {
@@ -37,9 +37,18 @@ app.post("/api/send-email", async (req, res) => {
       from: process.env.EMAIL_ID,
       to: process.env.EMAIL_TO,
       subject: "Info email",
-      text: `Name: ${requestData.name} Email: ${requestData.email} Contact: ${requestData.phone} Details: ${requestData.details}`
+      html: `
+              <div>
+                <h2>User Details</h2>
+                <p>The user provided the following details</p>
+                <ul>
+                  <li>Name: ${requestData.name}</li>
+                  <li>Email: ${requestData.email}</li>
+                  <li>Contact: ${requestData.phone}</li>
+                  <li>Message: ${requestData.details}</li>
+                </ul>
+              </div>`,  
     }
-    console.log(requestData);
 
     await new Promise((resolve, reject) => {
       transporter.sendMail(mailOptions, (error, info) => {
@@ -49,11 +58,11 @@ app.post("/api/send-email", async (req, res) => {
         }else{
           console.log("Email sent sucessfully", info.response);
           resolve(info);
+          res.status(200).json ( {message: "email sent", data: requestData});
         }
       })
     })
 
-    res.status(200).json ( {message: "email sent", data: requestData});
   }catch (error) {
     next(error)
   }finally {
